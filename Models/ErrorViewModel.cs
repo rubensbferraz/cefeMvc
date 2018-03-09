03a7db2f0913aefecea8d558678ff7466a17036b
rubensbferraz <rubensbferraz@gmail.com>
2018-03-09 07:04:32 -0300

    atualizacao

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CefeMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
